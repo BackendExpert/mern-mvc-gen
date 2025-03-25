776b14a3b1f8cda2e10bf4b8261bf79e22da1a0e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-25 07:45:10 +0530

    Create uniqueIdentifier.js

---
+++ src/fucntion/uniqueIdentifier.js
@@ -0,0 +1,35 @@
+const { getModel } = require("../utils/getModel")
+
+const uniqueIdentifier = (identifierFeild, modelname) => {
+    return async (req, res, next) => {
+        try{
+            const Model = getModel(modelname)
+
+            if(!Model){
+                return res.json({ Error: "Model Cannot be find"})
+            }
+    
+            const requestData = req.body
+    
+            if(requestData[identifierFeild]){
+                return res.json({ Error: `Missing ${identifierFeild} on Request Data (request body)` })
+            }
+    
+            const identifierValue = requestData[identifierFeild]
+    
+            const checkrecode = await Model.findOne({ [identifierFeild]: identifierValue })
+    
+            if(!checkrecode){
+                return res.json({ Error: `No ${modelname} by Given ${identifierFeild}...`})
+            }
+    
+            next()
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+
+}
+
+module.exports = { uniqueIdentifier }
