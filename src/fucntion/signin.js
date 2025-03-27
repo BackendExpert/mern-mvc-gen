32bb22b0e8f1735db57747d63b4d4beaae546793
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-03-27 12:05:16 +0530

    Update signin.js

--- src/fucntion/signin.js
+++ src/fucntion/signin.js
@@ -6,6 +6,28 @@ const signin = (modelname) => {
     return async(req, res) => {
         try{
             const Model = getModel(modelname)
+
+            if(!Model){
+                return res.json({ Error: "The Model cannot find in Project"})
+            }
+
+            const email = req.body.email
+
+            const checkemail = await Model.findOne({ email: email })
+
+            if(checkemail){
+                return res.json({ Error: "No User Found by Given Email"})
+            }
+
+            const checlpass = await bcrypt.compare(req.body.password, checkemail.password)
+
+            if(!checlpass){
+                return res.json({ Error: "Password Not Match..."})
+            }
+
+            const token = jwt.sign({ id: checkemail._id, role:checkemail.role }, process.env.JWT_SECRET);
+            return res.json({ Status: "Success", Result: checkemail, Token: token })
+
         }
         catch(err){
             console.log(err)
