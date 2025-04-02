f8c8de4b9abfa228b651ab00b77843220d76f47b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-02 10:03:17 +0530

    Update

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const { signup } = require('./src/fucntion/signup')
 const { signin } = require('./src/fucntion/signin')
 const { authMiddleware } = require('./src/middleware/authMiddleware')
 const { imageupload } = require('./src/middleware/ImageUploadMiddleware')
+const { accessMiddleware } = require('./src/middleware/accessMiddleware').default
 
-
-module.exports = { uniqueIdentifier, signup, signin, authMiddleware, imageupload }
+module.exports = { uniqueIdentifier, signup, signin, authMiddleware, imageupload, accessMiddleware }
